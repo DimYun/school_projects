23dd7df26bd70bae70f6b175c8a23b18f5d45d70
Draconx <dm.yunovidov@gmail.com>
2015-02-16 14:00:18 +0300

    Fix bug

--- Optical_microscope/with_qt.py
+++ Optical_microscope/with_qt.py
@@ -20,20 +20,22 @@ class Microscope_main(QtGui.QMainWindow):
     number_cam = 0
 
     def __init__(self, parent=None):
-        QtGui.QMainWindow.__init__(self, parent)
+        QtGui.QMainWindow.__init__(self, parent=None)
         desktop = QtGui.QApplication.desktop()
         x = desktop.width()
         y = desktop.height()
         #print x, y
         self.move(x / 2.0, y / 2.0)
+        #self.resize(1000, 1000)
         self.setWindowTitle('Optical microscope')
-        self.vl_main = QtGui.QVBoxLayout(self)
+        self.centralwidget = QtGui.QWidget(self)
+        self.vl_main = QtGui.QVBoxLayout(self.centralwidget)
         self.cb_camera = QtGui.QComboBox()
-        self.cb_camera.addItems(['1', '2', '3'])
+        self.cb_camera.addItems(['0', '1', '2', '3'])
         self.vl_main.addWidget(self.cb_camera)
         self.label = QtGui.QLabel()
-        self.vl_main.addWidget(self.label)
         #self.label.setGeometry(QtCore.QRect(80, 30, 491, 391))
+        self.vl_main.addWidget(self.label)
         self.hl_button = QtGui.QHBoxLayout()
         self.button_start = QtGui.QPushButton('Start video')
         self.button_start.setCheckable(True)
@@ -41,7 +43,7 @@ class Microscope_main(QtGui.QMainWindow):
         self.hl_button.addWidget(self.button_start)
         self.hl_button.addWidget(self.button_save)
         self.vl_main.addLayout(self.hl_button)
-
+        self.setCentralWidget(self.centralwidget)
         if self.button_start.isEnabled():
             self.start_video
         QtCore.QObject.connect(self.button_start, QtCore.SIGNAL('toggled(bool)'), self.start_video)
@@ -68,13 +70,13 @@ class Microscope_main(QtGui.QMainWindow):
 
     def show_frame(self):
         ipl_image = self.webcam.getImage()
-        ipl_image = ipl_image.crop(x=0, y=0, w=100, h=100)
+        #ipl_image = ipl_image.crop(x=0, y=0, w=100, h=100)
         ipl_image.dl().circle((150, 75), 50, Color.RED, filled = True)
         data = ipl_image.getBitmap().tostring()
         image = QtGui.QImage(data, ipl_image.width, ipl_image.height, 3 * ipl_image.width, QtGui.QImage.Format_RGB888)
         pixmap = QtGui.QPixmap()
         pixmap.convertFromImage(image.rgbSwapped())
-        self.MainWindow.label.setPixmap(pixmap)
+        self.label.setPixmap(pixmap)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
